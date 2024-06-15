636bb50bdf24dc96ffa6623a87b88920a5cbb8ed
Pedro Henrique <pedro.henrique.martins404@gmail.com>
2024-06-15 15:29:29 -0300

    feat: :art: Start Custom DashBoard and Navigation Component

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,15 +1,18 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
-
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 flex flex-col gap-3">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
+                    <h1 class="text-lg">Seja Bem-Vindo ao meu Teste de Back-End do <span class="bg-yellow-600 px-3 py-2 rounded text-white font-bold">Vokerê</span></h1>
+                </div>
+            </div>
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
+                    <h1 class="text-lg">Meus Materias de Estudo: </span></h1>
+                </div>
+                <div class="px-6 text-gray-900">
+                    <h1 class="text-sl">Modelo ER do Banco de Dados - Feito no BrModelo</span></h1>
+                    <img src="{{ url('images/sys_ER.png') }}" alt="Modelo Feito no BRmodelo sobre a Base de dados do Teste">
                 </div>
             </div>
         </div>
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -6,6 +6,7 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <title>{{ config('app.name', 'Laravel') }}</title>
+        <link rel="shortcut icon" href="{{ url('images/favicon.ico') }}" type="image/x-icon">
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
